a045fcd9431d81725734c3fa3297b6e0146e9179
Oleksandr <kruushik89@gmail.com>
2022-02-04 22:09:21 +0200

    add css grid for layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -4,23 +4,23 @@ import Sidebar from "./Sidebar/Sidebar";
 import Footer from "./Footer/Footer";
 import {Component, FunctionComponent} from "react";
 
+import styles from "./Layout.module.css";
+
 const Layout = ({children}: LayoutProps): JSX.Element => {
-  return (
-      <>
-        <Header/>
-        <div>
-          <Sidebar />
-          <div>
-            {children}
-          </div>
+    return (
+        <div className={styles.wrapper}>
+            <Header className={styles.header}/>
+            <Sidebar className={styles.sidebar}/>
+            <div className={styles.body}>
+                {children}
+            </div>
+            <Footer className={styles.footer}/>
         </div>
-        <Footer/>
-      </>
-  )
+    )
 }
 
 export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
-    return function withLayoutComponent (props: T): JSX.Element {
+    return function withLayoutComponent(props: T): JSX.Element {
         return (
             <Layout>
                 <Component {...props}/>
