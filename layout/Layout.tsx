ae2bb271dc2ba6ecd415184d4646399713786f0b
Oleksandr Krushynskyi <oleksandr.krushynskyi@teamvoy.com>
2022-02-16 14:36:21 +0200

    finish part context

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -5,6 +5,7 @@ import Footer from "./Footer/Footer";
 import {Component, FunctionComponent} from "react";
 
 import styles from "./Layout.module.css";
+import {AppContextProvider, IAppContext} from "../context/app.context";
 
 const Layout = ({children}: LayoutProps): JSX.Element => {
     return (
@@ -19,12 +20,15 @@ const Layout = ({children}: LayoutProps): JSX.Element => {
     )
 }
 
-export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
+// height order component
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(Component: FunctionComponent<T>) => {
     return function withLayoutComponent(props: T): JSX.Element {
         return (
+          <AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
             <Layout>
                 <Component {...props}/>
             </Layout>
+          </AppContextProvider>
         )
     }
 }
---
+++ layout/Menu/Menu.tsx
@@ -0,0 +1,74 @@
+import React, {useContext} from "react";
+import cn from "classnames";
+
+import {AppContext} from "../../context/app.context";
+import {FirstLevelMenuItem, PageItem} from "../../interfaces/menu.interface";
+import {TopLevelCategory} from "../../interfaces/page.interface";
+
+import CoursesIcon from "./icons/courses.svg";
+import ServicesIcon from "./icons/services.svg";
+import BooksIcon from "./icons/books.svg";
+import ProductsIcon from "./icons/products.svg";
+
+import styles from "./Menu.module.css";
+
+const firstLevelMenu: FirstLevelMenuItem[] = [
+  {route: "courses", name: "Курси", icon: <CoursesIcon/>, id: TopLevelCategory.Courses},
+  {route: "services", name: "Сервіси", icon: <ServicesIcon/>, id: TopLevelCategory.Services},
+  {route: "books", name: "Книги", icon: <BooksIcon/>, id: TopLevelCategory.Books},
+  {route: "products", name: "Продукти", icon: <ProductsIcon/>, id: TopLevelCategory.Products},
+]
+
+const Menu = (): JSX.Element => {
+  const {menu, setMenu, firstCategory} = useContext(AppContext);
+  const buildFirstLevel = () => {
+    return <>
+      {firstLevelMenu.map(m => (
+        <div key={m.route}>
+          <a href={`/${m.route}`}>
+            <div className={cn(styles.first_level, {
+              [styles.first_level__active]: m.id === firstCategory
+            })}>
+              {m.icon}
+              <span>{m.name}</span>
+            </div>
+          </a>
+          {m.id === firstCategory && buildSecondLevel(m.route)}
+        </div>
+      ))}
+    </>
+  }
+
+  const buildSecondLevel = (route: string) => {
+    return (
+      <div className={styles.second_block}>
+        {menu.map(m => (
+          <div key={m._id.secondCategory}>
+            <div className={cn(styles.second_level)}>{m._id.secondCategory}</div>
+            <div className={cn(styles.second_level__block, {
+              [styles.second_level__block__opened]: m.isOpened
+            })}>{buildThirdLevel(m.pages, route)}</div>
+          </div>
+        ))}
+      </div>
+    )
+  }
+
+  const buildThirdLevel = (pages: PageItem[], route: string) => {
+    return (
+        pages.map(page => <a href={`/${route}/${page.alias}`} key={page.title} className={cn(styles.third_level, {
+          [styles.third_level__active]: false
+        })}>
+          {page.category}
+        </a>)
+    )
+  }
+
+  return (
+    <div className={styles.menu}>
+      {buildFirstLevel()}
+    </div>
+  )
+}
+
+export default Menu;
